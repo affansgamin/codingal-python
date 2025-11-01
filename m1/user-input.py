e6fecf422302c212bdcfa92ccc8dbfae38fc13d8
affansgamin <amirahnaffan4@gmail.com>
2025-11-02 10:31:10 +1100

    Keywords and imports

---
+++ m1/user-input.py
@@ -0,0 +1,8 @@
+name = input('Enter your name: ')
+age = input('Enter your age: ')
+height = input('Enter your height: ')
+city = input('Enter the city you live in: ')
+
+print(f'Hello {name}. You are {age} years old. You are {height} tall. You live in the city {city}.')
+
+
