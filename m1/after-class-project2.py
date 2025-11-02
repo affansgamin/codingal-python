c766152ad17ad38691eccbcbe8c050cda1ec4313
affansgamin <amirahnaffan4@gmail.com>
2025-11-02 16:21:30 +1100

    after class porject

--- m1/after-class-project2.py
+++ m1/after-class-project2.py
@@ -1,7 +1,7 @@
-birthday1 = input('Enter the first persons age: ')
-birthday2 = input('Enter the second persons age: ')
-birthday3 = input('Enter the third persons age: ')
-birthday4 = input('Enter the fourth persons age: ')
-birthday5 = input('Enter the fifth persons age: ')
+birthday1 = input('Enter the first persons birthday: ')
+birthday2 = input('Enter the second persons birthday: ')
+birthday3 = input('Enter the third persons birthday: ')
+birthday4 = input('Enter the fourth persons birthday: ')
+birthday5 = input('Enter the fifth persons birthday: ')
 
 print(f"You have confirmed the age: {birthday1}, {birthday2}, {birthday3}, {birthday4}, {birthday5}")
