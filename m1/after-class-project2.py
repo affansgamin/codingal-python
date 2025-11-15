387012fd82065e44967f0f733f0ba93c85004457
affansgamin <amirahnaffan4@gmail.com>
2025-11-16 10:11:22 +1100

    after-class-project

--- m1/after-class-project2.py
+++ m1/after-class-project2.py
@@ -3,5 +3,3 @@ birthday2 = input('Enter the second persons birthday: ')
 birthday3 = input('Enter the third persons birthday: ')
 birthday4 = input('Enter the fourth persons birthday: ')
 birthday5 = input('Enter the fifth persons birthday: ')
-
-print(f"You have confirmed the age: {birthday1}, {birthday2}, {birthday3}, {birthday4}, {birthday5}")
---
+++ m1/after-class-project4.py
@@ -0,0 +1,7 @@
+temperature = float(input("Enter temperature in Celsius: "))
+if temperature < 20:
+    print("wear heavy clothes")
+elif temperature >=20 and temperature <=30:
+    print("wear normal clothes")
+else:
+    print("wear light clothes")
