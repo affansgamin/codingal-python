a16724ea0539d5e23fb4037de30c602715513540
affansgamin <amirahnaffan4@gmail.com>
2025-11-29 08:18:53 +1100

    profit

--- m1/mobile-data.py
+++ m1/mobile-data.py
@@ -1,8 +1,8 @@
 gb = int(input("Enter the mobile data in GB used: "))
 if gb <= 5:
-    cost = 100
+    cost = 100*gb
 elif gb >= 6 and gb <= 10:
-    cost = 100 + (gb - 5)*20
+    cost = 100*5 + (gb - 5)*120
 elif gb >= 11:
-    cost = 100 + (gb - 10)*30 + 5*20
+    cost = 100*5 + 120*5 + (gb - 10)*130
 print(f"The total mobile data cost is: ${cost}")
