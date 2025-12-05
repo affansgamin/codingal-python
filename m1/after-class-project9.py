fae5e37ea4b30a036574b19b4c41ca2e0292b807
affansgamin <amirahnaffan4@gmail.com>
2025-12-05 21:30:53 +1100

    after-class-project

---
+++ m1/after-class-project9.py
@@ -0,0 +1,4 @@
+number = int(input("Enter a number: "))
+power = int(input("Enter the power to raise the number to: "))
+result = number ** power
+print(f"{number} raised to the power of {power} is {result}")
