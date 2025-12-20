667aea673b46fa6b00b46d31f86213b05122594e
affansgamin <amirahnaffan4@gmail.com>
2025-12-21 09:54:40 +1100

    functions

--- m1/simple-calc.py
+++ m1/simple-calc.py
@@ -10,19 +10,40 @@ def mod(n, n2):
     return n %n2
 def exp(n, n2):
     return n ** n2
-n = int(input("enter the first number: "))
-n2 = int(input("enter the second number: "))
-print('0. quit')
-print('1. add')
-print('2. subtract')
-print('3. divide')
-print('4. multiply')
-print('5. mod')
-print('6. exponentiation')
-option = int(input("please choose an option from 0-6 : "))
-print(f'sum = {add(n, n2)}')
-print(f'subtraction = {subtract(n, n2)}')
-print(f'division = {divide(n, n2)}')
-print(f'multiplication = {multiply(n, n2)}')
-print(f'mod = {mod(n, n2)}')
-print(f'exponentiation = {exp(n, n2)}')
+while True:
+    n = int(input("enter the first number: "))
+    n2 = int(input("enter the second number: "))
+    print('0. quit')
+    print('1. add')
+    print('2. subtract')
+    print('3. divide')
+    print('4. multiply')
+    print('5. mod')
+    print('6. exponentiation') 
+
+    option = int(input("please choose an option from 0-6 : "))
+
+    if option == 0:
+        print('exiting the program...')
+        break
+    elif option == 1:
+        print(f'sum = {add(n, n2)}')
+        break
+    elif option == 2:
+        print(f'subtraction = {subtract(n, n2)}')
+        break
+    elif option == 3:
+        print(f'division = {divide(n, n2)}')
+        break
+    elif option == 4:
+        print(f'multiplication = {multiply(n, n2)}')
+        break
+    elif option == 5:
+        print(f'mod = {mod(n, n2)}')
+        break
+    elif option == 6:
+        print(f'exponentiation = {exp(n, n2)}')
+        break
+    else:
+        print('invalid input')
+        break
